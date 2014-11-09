b63e5f80022feb98a2f8d7a8bbb021794814cd03
kykdev <kykint@naver.com>
2014-11-09 19:09:04 +0900

    Nov 8, 2014 - SM 4.9.3
    Signed-off-by: kykdev <kykint@naver.com>

--- lib/gcc/arm-eabi/4.9.3/plugin/include/plugin-version.h
+++ lib/gcc/arm-eabi/4.9.3/plugin/include/plugin-version.h
@@ -6,7 +6,7 @@
 #define GCCPLUGIN_VERSION  (GCCPLUGIN_VERSION_MAJOR*1000 + GCCPLUGIN_VERSION_MINOR)
 
 static char basever[] = "4.9.3";
-static char datestamp[] = "20141102";
+static char datestamp[] = "20141108";
 static char devphase[] = "prerelease";
 static char revision[] = "";
 
